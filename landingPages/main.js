56a310c0e53be2c441fd77dc348a4df2d27bbe62
Tamsauce <thughes78@gmail.com>
2021-08-28 02:07:55 -0700

    move files

--- landingPages/main.js
+++
@@ -1,9 +0,0 @@
-$("#arrow").click(function() {
-    doBounce($(this), 3, '10px', 300);   
-});
-function doBounce(element, times, distance, speed) {
-    for(var i = 0; i < times; i++) {
-        element.animate({marginTop: '-='+distance}, speed)
-            .animate({marginTop: '+='+distance}, speed);
-    }        
-}
